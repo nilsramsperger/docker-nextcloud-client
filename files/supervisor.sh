d6924edc48b542324d40c6b9a0476ed0c05655e6
Nils Ramsperger <nils@ramsperger-home.de>
2019-11-18 16:24:13 +0100

    Set sync interval to 1 minute.

--- files/supervisor.sh
+++ files/supervisor.sh
@@ -11,6 +11,8 @@ trap term_handler SIGTERM
 
 while true
 do
+    echo "Start sync"
 	/bin/su -s /bin/ash nextcloud -c "nextcloudcmd --non-interactive -u '$USER' -p '$PSWD' /home/nextcloud/data '$HOST'"
-	sleep 5000
+    echo "Sync done"
+	sleep 60000
 done
