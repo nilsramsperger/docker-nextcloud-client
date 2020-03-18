8f1e5af55648f4befe1b9ff3f56643c9a915f2c5
Nils Ramsperger <nils@ramsperger-home.de>
2020-03-18 13:14:49 +0100

    Removed chown Added chmod 755 for files before sync.

--- files/supervisor.sh
+++ files/supervisor.sh
@@ -12,7 +12,7 @@ trap term_handler SIGTERM
 while true
 do
     echo "Start sync"
-    chown -R nextcloud:nextcloud /home/nextcloud/data
+    chmod -R 755 /home/nextcloud/data
     /bin/su -s /bin/ash nextcloud -c "nextcloudcmd --non-interactive -u '$USER' -p '$PSWD' /home/nextcloud/data '$HOST'"
     echo "Sync done"
     sleep 60
